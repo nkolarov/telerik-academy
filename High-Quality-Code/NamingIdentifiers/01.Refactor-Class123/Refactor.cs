b40ff27107f86813cd00ade813e9d5aaa605ee44
nkolarov <nikolaikolarov@gmail.com>
2013-04-17 05:57:24 -0700

    Some refactoring. Added comments.

---
+++ High-Quality-Code/NamingIdentifiers/01.Refactor-Class123/Refactor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/// <summary>
+/// Shows correct naming
+/// </summary>
+public class Refactor
+{
+    /// <summary>
+    /// Stupid and unused constant.
+    /// </summary>
+    public const int MaxCount = 6;
+
+    /// <summary>
+    /// Fun starts here.
+    /// </summary>
+    public static void Main()
+    {
+        Refactor.Printer instance = new Refactor.Printer();
+        instance.Print(true);
+    }
+
+    /// <summary>
+    /// Inner class for printing boolean variable to the console.
+    /// </summary>
+    private class Printer
+    {
+        /// <summary>
+        /// Print <paramref name="isStupidCode"/> to the Console
+        /// </summary>
+        /// <param name="isStupidCode">Some boolean variable.</param>
+        public void Print(bool isStupidCode)
+        {
+            string isStupidCodeAsString = isStupidCode.ToString();
+            Console.WriteLine(isStupidCodeAsString);
+        }
+    }
+}
