11a5cfbb44eb406bde38b2b147cfb8a0650a1a31
nkolarov <nikolaikolarov@gmail.com>
2013-05-01 09:44:39 -0700

    Added AdvancedFunctions

---
+++ JavaScript/AdvancedFunctions/scripts/domModule.js
@@ -0,0 +1,78 @@
+﻿/*    01. Create a module for working with DOM. The module should have the following functionality
+        Add DOM element to parent element given by selector
+        Remove element from the DOM  by given selector
+        Attach event to given selector by given event type and event hander
+        Add elements to buffer, which appends them to the DOM when their count for some selector becomes 100
+        The buffer contains elements for each selector it gets
+        Get elements by CSS selector
+        The module should reveal only the methods.*/
+
+var domModule = function () {
+
+    /**
+    * Appends a child element to a parent by given selector.
+    */
+    var appendChild = function (childType, selector) {
+        // More info @: https://developer.mozilla.org/en-US/docs/DOM/Node.appendChild
+        document.querySelector(selector).appendChild(childType);
+    }
+
+    /**
+    * Removes a child element by given selector.
+    */
+    var removeChild = function (parent, selector) {
+        // More info @: https://developer.mozilla.org/en-US/docs/DOM/Node.removeChild
+        var elementSelector = parent + " " + selector;
+        var childs = document.querySelectorAll(elementSelector);
+
+        for (var i = 0; i < childs.length; i++) {
+            childs[i].parentNode.removeChild(childs[i]);
+        }
+    }
+
+    /**
+    * Attaches an event of an element by given element selector, event type and event handler.
+    */
+    var addHandler = function (selector, type, handler) {
+        // More info @: https://developer.mozilla.org/en-US/docs/DOM/EventTarget.addEventListener
+        var elements = document.querySelectorAll(selector);
+        var useCapture = false;
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].addEventListener(type, handler, useCapture);
+        }
+    }
+
+    var buffer = [];
+    var BUFFER_MAX_SIZE = 5;
+
+    /**
+    * Flushes the buffer by appending all elements from the buffer to their parents.
+    */
+    var flushBuffer = function () {
+        for (var i = 0; i < buffer.length; i++) {
+            var parent = buffer[i].parent;
+            var element = buffer[i].element;
+            parent.appendChild(element);
+        }
+        buffer = [];
+    }
+
+    /**
+    * Appends element to a buffer by given parent selector and element.
+    */
+    var appendToBuffer = function (selector, element) {
+        var parent = document.querySelector(selector);
+        buffer.push({ parent: parent, element: element });
+
+        if (buffer.length === BUFFER_MAX_SIZE) {
+            flushBuffer();
+        }
+    }
+
+    return {
+        appendChild: appendChild,
+        removeChild: removeChild,
+        addHandler: addHandler,
+        appendToBuffer: appendToBuffer
+    }
+}();
---
+++ JavaScript/AdvancedFunctions/scripts/specialConsole.js
@@ -0,0 +1,84 @@
+﻿/* 03. Create a module to work with the console object.Implement functionality for:
+Writing a line to the console 
+Writing a line to the console using a format
+Writing to the console should call toString() to each element
+Writing errors and warnings to the console with and without format
+*/
+
+var specialConsole = function () {
+    /**
+    * Checks if the function is invoked with more than one parameter (there are placeholders).
+    */
+    function hasPlaceholders(args) {
+        if (args.length > 1) {
+            return true;
+        }
+    }
+
+    /**
+    * Replaces all placeholders in a string.
+    * The string is the first element of args Array followed by placeholders data.
+    */
+    function replacePlaceholders(args) {
+        var placeholderData = [];
+        var resultString = args[0];
+
+        for (var i = 0; i < args.length - 1; i++) {
+            placeholderData[i] = args[i + 1];
+        }
+
+        for (var j = 0; j < placeholderData.length; j++) {
+            var token = "\\{" + j + "\\}";
+            var regex = new RegExp(token, "g");
+            resultString = resultString.replace(regex, placeholderData[j]);
+        }
+
+        return resultString;
+    }
+    
+    /**
+    * Checks for placeholders. Replaces them and returns human readable message.
+    */
+    function prepareMessage(args) {
+        var message = "";
+
+        if (hasPlaceholders(args)) {
+            message = replacePlaceholders(args);
+        }
+        else {
+            message = args[0];
+        }
+
+        return message;
+    }
+
+    /**
+    * Writes a message to a new console line.
+    */
+    function writeLine() {
+        var message = prepareMessage(arguments);
+        console.log(message);
+    }
+
+    /**
+    * Writes an error message to the console.
+    */
+    function writeError() {
+        var message = prepareMessage(arguments);
+        console.error(message);
+    }
+   
+    /**
+    * Writes a warning message to the console.
+    */
+    function writeWarning() {
+        var message = prepareMessage(arguments);
+        console.warn(message);
+    }
+
+    return {
+        writeLine: writeLine,
+        writeError: writeError,
+        writeWarning: writeWarning
+    }
+}()
