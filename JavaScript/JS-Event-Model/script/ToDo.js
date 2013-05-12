6c4bd64de1a9f463fe23e5a9c0f0206ff4ef912d
nkolarov <nikolaikolarov@gmail.com>
2013-05-12 11:54:24 -0700

    Added JS-Event-Model.

---
+++ JavaScript/JS-Event-Model/script/ToDo.js
@@ -0,0 +1,94 @@
+﻿/*
+02. Create a TODO list with the following UI controls
+- Form input for new Item
+- Button for adding the new Item
+- Button for deleting some item
+- Show and Hide Button
+*/
+
+var addButton = document.getElementById("add");
+var removeButton = document.getElementById("remove");
+var hideButton = document.getElementById("hide");
+var showButton = document.getElementById("show");
+
+/*
+* Prepares a new checkbox element.
+*/
+function initializeToDoTask(currentTask) {
+    var check = document.createElement("input");
+    check.type = "checkbox";
+    check.name = "checkbox";
+
+    // http://stackoverflow.com/questions/6293588/how-to-create-an-html-checkbox-with-a-clickable-label
+    var task = document.createElement("label");
+    task.style.display = "block";
+    task.appendChild(check);
+    task.appendChild(document.createTextNode(currentTask));
+
+    return task;
+}
+
+/*
+* Adds a new task to the list.
+*/
+function addTask() {
+    var currentTask = document.getElementById("task");
+    var currentTaskValue = currentTask.value;
+    var list = document.getElementById("list");
+
+    if (currentTaskValue == "" || currentTaskValue == null) {
+        alert("Please Enter a Task!");
+        return;
+    }
+
+    var currentToDoTask = initializeToDoTask(currentTaskValue);
+    list.appendChild(currentToDoTask);
+
+    // Clear task input element.
+    currentTask.value = "";
+}
+
+/*
+* Removes all selected tasks.
+*/
+function removeSelectedTasks() {
+    var selectedCheckboxes = document.querySelectorAll("input[type=checkbox]:checked");
+
+    for (var i = 0; i < selectedCheckboxes.length; i++) {
+        var currentTask = selectedCheckboxes[i];
+        var currentTaskLabel = currentTask.parentNode;
+        if (currentTaskLabel.style.display == "block") {
+            currentTaskLabel.parentNode.removeChild(currentTaskLabel);
+        }
+    }
+}
+
+/*
+* Hides all selected tasks.
+*/
+function hideSelectedTasks() {
+    var selectedCheckboxes = document.querySelectorAll("input[type=checkbox]:checked");
+
+    for (var i = 0; i < selectedCheckboxes.length; i++) {
+        selectedCheckboxes[i].parentNode.style.display = "none";
+    }
+}
+
+/*
+* Shows all selected tasks.
+*/
+function showHiddenTasks() {
+    var checkboxes = document.querySelectorAll("input[type=checkbox]");
+
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].parentNode.style.display == "none") {
+            checkboxes[i].parentNode.style.display = "block";
+        }
+
+    }
+}
+
+addButton.addEventListener("click", addTask, false);
+removeButton.addEventListener("click", removeSelectedTasks, false);
+hideButton.addEventListener("click", hideSelectedTasks, false);
+showButton.addEventListener("click", showHiddenTasks, false);
---
+++ JavaScript/JS-Event-Model/script/carousel.js
@@ -0,0 +1,80 @@
+﻿/* 01. Create a Simple JS Carousel with N images and two arrows for image control */
+var carousel = function () {
+    var images = [];
+    var imageIndex = 0;
+
+    /*
+    * Represents an enumeration for avaiable image directions.
+    */
+    var imageDirection = Object.freeze({
+        "LEFT": 0,
+        "RIGHT": 1
+    });
+
+    /*
+    * Adds an image by given relative path to a parent element defined with parent selector.
+    */
+    function addImage(parentSlector, relativePathToImage) {
+        var parent = document.querySelector(parentSlector);
+        var image = document.createElement("img");
+
+        image.setAttribute("src",relativePathToImage);
+        parent.appendChild(image);
+        images.push(image);
+        // Hide all but first image
+        if (images.length - 1 > 0) {
+            hideImage(images.length - 1);
+        }
+    }
+
+    /*
+    * Checks if imageIndex is out of the permitted range and corrects it if needed.
+    */
+    function correctImageIndex() {
+        if (imageIndex < 0) {
+            imageIndex = images.length - 1;
+        }
+        else if (imageIndex > images.length - 1) {
+            imageIndex = 0;
+        }
+    }
+
+    /*
+    * Hides an image by given index.
+    */
+    function hideImage(index) {
+        images[index].style.display = "none";
+    }
+
+    /*
+    * Shows an image by given index.
+    */
+    function showImage(index) {
+        images[index].style.display = "";
+    }
+
+    /*
+    * Shows next image according to the passed direction.
+    */
+    function showNextImage(direction) {
+        hideImage(imageIndex);
+        switch (direction) {
+            case imageDirection.LEFT:
+                imageIndex--;
+                break;
+            case imageDirection.RIGHT:
+                imageIndex++;
+                break;
+            default:
+                throw new Error("Unknown Direction!");
+        }
+        correctImageIndex();
+        showImage(imageIndex);
+    }
+
+    return {
+        addImage: addImage,
+        showNextImage: showNextImage,
+        imageDirection: imageDirection
+    }
+}();
