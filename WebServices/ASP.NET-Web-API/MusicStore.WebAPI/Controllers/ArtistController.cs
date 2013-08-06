56c9ea151ed2d31af0f1eba29c5ad8e3367ea9cb
nkolarov <nikolaikolarov@gmail.com>
2013-08-06 14:57:00 -0700

    Added WebServices and web-API.

---
+++ WebServices/ASP.NET-Web-API/MusicStore.WebAPI/Controllers/ArtistController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using MusicStore.Model;
+using MusicStore.Data;
+
+namespace MusicStore.WebAPI.Controllers
+{
+    public class ArtistController : ApiController
+    {
+        private MusicStoreContext db = new MusicStoreContext();
+
+        // GET api/Artist
+        public IEnumerable<Artist> GetArtists()
+        {
+            return db.Artists.AsEnumerable();
+        }
+
+        // GET api/Artist/5
+        public Artist GetArtist(int id)
+        {
+            Artist artist = db.Artists.Find(id);
+            if (artist == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return artist;
+        }
+
+        // PUT api/Artist/5
+        public HttpResponseMessage PutArtist(int id, Artist artist)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            if (id != artist.ArtistId)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            db.Entry(artist).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // POST api/Artist
+        public HttpResponseMessage PostArtist(Artist artist)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Artists.Add(artist);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, artist);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = artist.ArtistId }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        // DELETE api/Artist/5
+        public HttpResponseMessage DeleteArtist(int id)
+        {
+            Artist artist = db.Artists.Find(id);
+            if (artist == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Artists.Remove(artist);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, artist);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebServices/ASP.NET-Web-API/readMe.txt
@@ -0,0 +1,9 @@
+01. Using ASP.NET Web API and Entity Framework (database first or code first) create a database and web services with full CRUD (create, read, update, delete) operations for hierarchy of following classes:
+Artists (Name, Country, DateOfBirth, etc.)
+Albums (Title, Year, Producer, etc.)
+Songs (Title, Year, Genre, etc.)
+Every album has a list of artists
+Every song has artist
+Every album has list of songs
+
+02. Create console application and demonstrate the use of all service operations using the HttpClient class (with both JSON and XML)
